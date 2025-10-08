7ce4b03646157c7b34571491785fde8302b340ab
laragamasantos <laragamasantos@gmail.com>
2025-10-07 21:18:24 -0300

    generate graphic

---
+++ create_graphic.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+
+
+plt.style.use('seaborn-darkgrid')
+plt.rcParams.update({'figure.figsize': (10,6), 'font.size': 12})
+
+csv_path = "ocupacao_buffer.csv"
+df = pd.read_csv(csv_path)
+
+x = df['operacao']
+y = df['ocupacao']
+
+plt.figure()
+plt.plot(x, y, linewidth=1, color='tab:blue')
+plt.xlabel('Operação')
+plt.ylabel('Ocupação do buffer')
+plt.title('Ocupação do buffer ao longo das operações')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('ocupacao_buffer_line.png', dpi=150)
+print("Salvo: ocupacao_buffer_line.png")
+
+plt.figure()
+plt.hist(y, bins=range(0, max(y)+2), color='tab:green', edgecolor='black', align='left')
+plt.xlabel('Ocupação do buffer')
+plt.ylabel('Frequência')
+plt.title('Distribuição da ocupação do buffer')
+plt.tight_layout()
+plt.savefig('ocupacao_buffer_hist.png', dpi=150)
+print("Salvo: ocupacao_buffer_hist.png")
+
+plt.show()
